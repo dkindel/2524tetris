6f754dc80168e19a2ef9115b68f59160f6d87aa3
Dave Kindel <dkindel@vt.edu>
2013-04-15 14:03:24 -0400

    added event handlers for up, left, right, and escape, added testmatrices

--- Tetris.py
+++ Tetris.py
@@ -2,42 +2,39 @@
 # ECE 2524 Final Project - Tetris
 
 from Tkinter import *
+from TestMatrix import *
 import random
 
 class Application(Frame):
-
-  #//def redraw(self):
-    #self.
  
   def initDraw(self):
-    matrix2 = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[1,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0], \
-	      [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0], \
-	      [0,0,0,0,0,0,0,0,0,0],[2,0,0,1,0,1,0,2,0,0],[2,0,0,1,2,1,0,1,1,1],[1,0,1,1,0,1,0,1,0,1],[0,0,0,0,0,0,0,0,0,0]]
-
-
+    
     self.board = Canvas(self, bg = 'gray', height = self.boardHeight, width = self.boardWidth, bd = 0)
     self.chcol = Button(self, bg = 'red')
-    self.chcol["command"] = self.redrawBoard
+    self.chcol["command"] = self.changeMatrix
     self.chcol["text"] = "Redraw"
     self.chcol.pack(side = BOTTOM)
     
     self.redrawBoard()
     self.board.pack()
-    self.board.after(1000, self.changeMatrix, matrix2)
+    #self.board.after(1000, self.changeMatrix)
     
   def __init__(self, master=None):
+    root.bind_all('<Left>', self.keyleft)
+    root.bind_all('<Right>', self.keyRight)
+    root.bind_all('<Up>', self.keyUp)
+    root.bind_all('<Escape>', self.keyEscape)
     Frame.__init__(self, master)
-    self.boardHeight = 600
-    self.boardWidth = 400
+    self.boardHeight = 400
+    self.boardWidth = 250
     master.minsize(height = self.boardHeight+5, width = self.boardWidth +5)
     master.maxsize(height = self.boardHeight+5, width = self.boardWidth +5)
     self.sqSize = 25
     self.rows = 15
     self.cols = 10
-    self.matrix = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0], \
-	      [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0], \
-	      [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
-
+    self.matrixNum = 0
+    self.matrix = getNewMatrix(self.matrixNum)
+    
     master.title("Tetris!")
     self.pack()
     self.initDraw()
@@ -58,10 +55,24 @@ class Application(Frame):
 	else:
 	  self.board.create_rectangle(x1,y1,x2,y2, fill= 'orange')
     
-  def changeMatrix(self, matrix):
-    self.matrix = matrix
-    self.redrawBoard
+  def changeMatrix(self):
+    self.matrixNum = (self.matrixNum + 1) % getNumMatrices()
+    print self.matrixNum
+    self.matrix = getNewMatrix(self.matrixNum)
+    self.redrawBoard()
     
+    
+  def keyleft(self, event):
+    self.changeMatrix()
+    
+  def keyUp(self, event):
+    print 'up'
+    
+  def keyRight(self, event):
+    print 'right'
+    
+  def keyEscape(self, event):
+    root.quit()
 
 root = Tk()
 app = Application(master = root)
